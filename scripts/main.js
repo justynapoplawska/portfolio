e734cf6dbdc704ae7f21f1700a92509c77e9869c
dcampagne <dcampagne@gmail.com>
2016-07-18 20:42:27 +0100

    Update 2016-07-18T19:42:25.628Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,42 @@
-var currentHash="/";window.location.hash=currentHash,$(".main").delegate("a","click",function(){var o=$(this).attr("href");return window.location.hash="/"+o,$(".overlay__header i").html($(this).find($("h5")).text()),$(".overlay__content").load($(this).attr("href")),$(".overlay").fadeIn(),$("html, body").addClass("overflow-hidden"),!1}),$(".overlay__close").click(function(){$(".overlay").fadeOut(),$("html, body").removeClass("overflow-hidden"),window.location.hash="/",$(".overlay__content").empty()}),$(window).on("hashchange",function(){});
+
+var currentHash = '/';
+window.location.hash = currentHash;
+
+$('.main').delegate('a', 'click', function () {
+  var currentHash = $(this).attr('href');
+  window.location.hash = '/' + currentHash;
+  $('.overlay__header i').html($(this).find($('h5')).text());
+  $('.overlay__content').load($(this).attr('href'));
+  $('.overlay').fadeIn("slow");
+  $('html, body').addClass('overflow-hidden');
+  return false;
+});
+
+$('.overlay__close').click(function () {
+  $('.overlay').fadeOut();
+  $('html, body').removeClass('overflow-hidden');
+  window.location.hash = '/';
+  $('.overlay__content').empty();
+});
+
+$(window).on('hashchange', function () {
+  if (currentHash !== '') {} else {};
+});
+
+// var newHash     = '',
+//    $overlay = $('.overlay');
+
+// $('.main').delegate('a', 'click', function() {
+//   window.location.hash = $(this).attr('href');
+//   $('.overlay__header i').html($(this).find($('h5')).text());
+//   $('.overlay__content').load($(this).attr('href'));
+//   $('.overlay').fadeIn();
+//   $('html, body').addClass('overflow-hidden');
+//   return false;
+// });
+
+// $( ".overlay__close" ).click(function() {
+//   $( ".overlay" ).fadeOut();
+//   $('html, body').removeClass('overflow-hidden');
+// });
+//# sourceMappingURL=main.js.map
