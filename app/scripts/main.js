8f34a2644ba8106263dbde542c4a597d96e24c24
dcampagne <dcampagne@gmail.com>
2016-07-18 18:56:26 +0100

    fixed content trigger

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,27 @@
 
+var currentHash = '';
+var nowClick;
 $('.main').delegate('a', 'click', function() {
-   window.location.hash = $(this).attr('href');
-   return false;
+  var currentHash = $(this).attr('href');
+  window.location.hash = currentHash;
+ $('.overlay__header i').html($(this).find($('h5')).text());
+ $('.overlay__content').load($(this).attr('href'));
+ $('.overlay').fadeIn();
+ $('html, body').addClass('overflow-hidden');
+  return false;
+});
+
+$( ".overlay__close" ).click(function() {
+  $( ".overlay" ).fadeOut();
+  $('html, body').removeClass('overflow-hidden');
 });
 
 $(window).on('hashchange',function(){ 
-  if (window.location.hash !== '') {
-    alert('content');
+  if (currentHash !== '') {
   } else {
-    alert('welcome home');
   };
 });
 
-function showContent(page) {
-  alert(page);
-}
 // var newHash     = '',
 //    $overlay = $('.overlay');
 
